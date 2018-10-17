717754dffba20e789ef890fc959ceaa5b793e0c0
Atsuki <piacere.atsuki@gmail.com>
2018-10-17 20:29:17 +0900

    DSMC法，改良版

---
+++ mydef.h
@@ -0,0 +1,20 @@
+//
+// Created by 服部篤樹 on 2018/10/17.
+//
+
+#ifndef DSMC_RECT_MYDEF_H
+#define DSMC_RECT_MYDEF_H
+
+#pragma once
+
+
+#define nmax 300000						//変動する粒子の個数を十分保持できる程度に大きく取る
+//格子の数
+#define mx 100
+#define my 100
+#define mdx 200
+#define mdy 200
+#define B_mx 50
+#define B_my 50
+
+#endif //DSMC_RECT_MYDEF_H
